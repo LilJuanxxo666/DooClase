06e967ac2292dc3cbf3748251ca1450b6f3a167d
Juan Fernando <wuanter.castano@gmail.com>
2023-04-22 22:16:45 -0500

    Segundo commit de publiUco, con los utils

---
+++ .metadata/.plugins/org.eclipse.core.resources/.projects/PubliUcoCrossCutting/org.sonarlint.eclipse.core/issues/index.pb
@@ -0,0 +1,9 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+f
+6src/co/edu/uco/publiuco/crosscutting/package-info.java,7\8\78e9d80018f34bf9e7d96a0e5bd1fc0aab0281d8
+j
+:src/co/edu/uco/publiuco/crosscutting/utils/UtilObject.java,7\e\7e6c0023c8f13d32fa9c42d4c811258983f0f369
+h
+8src/co/edu/uco/publiuco/crosscutting/utils/UtilText.java,1\6\165917753e12908198950430169d560466096635
--- PubliUcoDTO/src/co/edu/uco/publiuco/dto/EstadoTipoRelacionInstitucionDTO.java
+++ PubliUcoDTO/src/co/edu/uco/publiuco/dto/EstadoTipoRelacionInstitucionDTO.java
@@ -2,6 +2,8 @@ package co.edu.uco.publiuco.dto;
 
 import java.util.UUID;
 
+import co.edu.uco.publiuco.crosscutting.utils.UtilText;
+
 public final class EstadoTipoRelacionInstitucionDTO {
 	
 	private UUID identificador;
@@ -10,12 +12,16 @@ public final class EstadoTipoRelacionInstitucionDTO {
 	
 	public EstadoTipoRelacionInstitucionDTO() {
 		super();
-		setIdentificador(identificador);
-		setNombre("");
-		setDescripcion("");
+		setIdentificador(UtilUUID.DEFAULT_UUID);
+		setNombre(UtilText.getUtilText().getDefaultValue());
+		setDescripcion(UtilText.EMPTY);
 	}
 	
-	public EstadoTipoRelacionInstitucionDTO(UUID identificador, String nombre, String descripcion) {
+	public static EstadoTipoRelacionInstitucionDTO create() {
+		return new EstadoTipoRelacionInstitucionDTO();
+	}
+	
+	public EstadoTipoRelacionInstitucionDTO(final UUID identificador, final String nombre, final String descripcion) {
 		super();
 		setIdentificador(identificador);
 		setNombre(nombre);
@@ -25,25 +31,40 @@ public final class EstadoTipoRelacionInstitucionDTO {
 		return identificador;
 	}
 	public final EstadoTipoRelacionInstitucionDTO setIdentificador(final UUID identificador) {
-		this.identificador = identificador;
+		this.identificador = UtilUUID.getDefault(identificador);
 		return this;
 	}
 	public final String getNombre() {
 		return nombre;
 	}
 	public final EstadoTipoRelacionInstitucionDTO setNombre(final String nombre) {
-		this.nombre = "";
-		
-		if(nombre != null) {
-			this.nombre = nombre.trim();
-		}
+		this.nombre = UtilText.getUtilText().applyTrim(nombre);
 		return this;
 	}
 	public final String getDescripcion() {
 		return descripcion;
 	}
 	public final EstadoTipoRelacionInstitucionDTO setDescripcion(final String descripcion) {
-		this.descripcion = descripcion;
+		this.descripcion = UtilText.getUtilText().applyTrim(descripcion);
 		return this;
+	}
+
+	@Override
+	public String toString() {
+		return "EstadoTipoRelacionInstitucionDTO [identificador=" + identificador + ", nombre=" + nombre
+				+ ", descripcion=" + descripcion + "]";
+	}
+	
+	public static void main(String[] args) {
+		EstadoTipoRelacionInstitucionDTO objeto = new EstadoTipoRelacionInstitucionDTO();
+		objeto.setIdentificador(UtilUUID.generateNewUUID());
+		objeto.setNombre("Docente");
+		objeto.setDescripcion("Soy Docente");
+		System.out.println(objeto);
+		
+		EstadoTipoRelacionInstitucionDTO objetoDos = 
+				EstadoTipoRelacionInstitucionDTO.create().setDescripcion("Soy docente")
+				.setIdentificador(UtilUUID.generateNewUUID()).setNombre("Docente");
+		System.out.println(objetoDos);
 	}
 }
